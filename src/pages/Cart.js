1bb7bf692ae80b7773a0b6d842bec17aece30c9c
Sanjaykumar Kamble <kamblesanjaykumar.n@gmail.com>
2022-07-10 16:25:49 +0530

    Updated

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { remove } from '../store/cartSlice';
 import { useNavigate } from 'react-router-dom';
-// import { useEffect } from 'react';
+
 
 
 
